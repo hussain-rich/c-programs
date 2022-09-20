2d60fd056da37af7f8c7cd880312a3b97f047e32
Hameed Hussain <101456259+hussain-rich@users.noreply.github.com>
2022-09-20 19:37:33 +0530

    keywords in c

---
+++ keywords.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char name[20]=" HAMEED HUSSAIN";
+    char fathernam[14]=" MOHAMED YUSUF";
+    float height=5.9;
+    int weight=58;
+    int weights=75;
+    const double pi=3.14;
+    printf("My name is %s",name);
+    printf("\nMy height is %f",height);
+
+    printf("\nMy weight before gym is %d kg",weight);
+    printf("\nMy current weight is %d",weights);
+    printf("\nThe value of pi is %lf",pi);
+    getch();
+}
